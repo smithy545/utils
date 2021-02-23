52d4f8958e306188f2f127aa4777e793c2169708
Philip Smith <smithy545@gmail.com>
2021-02-22 21:38:54 -0600

    Near perfect function

---
+++ algorithm_util.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Philip Smith on 2/18/2021.
+//
+
+#include "algorithm_util.h"
+
+namespace utils::alg {
+
+} // namespace utils::alg
---
+++ algorithm_util.h
@@ -0,0 +1,12 @@
+//
+// Created by Philip Smith on 2/18/2021.
+//
+
+#ifndef PHILUTIL_ALGORITHM_UTIL_H
+#define PHILUTIL_ALGORITHM_UTIL_H
+
+namespace utils::alg {
+    // TODO: simple-tree -> binary search tree -> red-black tree
+} // namespace utils::alg
+
+#endif //PHILUTIL_ALGORITHM_UTIL_H

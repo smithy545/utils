8f8e1f2e08e53ed32f1f12db3c24694c268b139e
Philip Smith <smithy545@gmail.com>
2021-06-28 00:36:38 -0500

    Implement textured buttons (naive way). TODO: Make virtual triggerable, collidable, and textured InterfaceElement base classes that provide some generic functionality to avoid duplicate code.

--- src/file_util.cpp
+++ src/file_util.cpp
@@ -40,6 +40,7 @@ namespace utils::file {
     }
 
     GLuint read_png_file_to_texture(const std::string &path) {
+        // Reference: http://www.libpng.org/pub/png/libpng-1.4.0-manual.pdf
         FILE* fp = fopen(path.c_str(), "rb");
         if(!fp) {
             std::string message = fmt::format("Error reading png file at {0}", path);
@@ -75,26 +76,68 @@ namespace utils::file {
             png_init_io(png_ptr, fp);
             auto png_transforms = PNG_TRANSFORM_IDENTITY;
             png_read_png(png_ptr, info_ptr, png_transforms, NULL);
-            // Todo: find a way to pass info_ptr->row_pointers directly to glTexImage2D without moving to data[]
-            auto w = info_ptr->width;
-            auto h = info_ptr->height;
-            auto* data = new unsigned char[h*info_ptr->rowbytes];
-            for(int y = 0; y < h; y++) {
-                for(int x = 0; x < info_ptr->rowbytes; x += 4) {
-                    data[y*info_ptr->rowbytes + x] = info_ptr->row_pointers[y][x];
-                    data[y*info_ptr->rowbytes + x + 1] = info_ptr->row_pointers[y][x+1];
-                    data[y*info_ptr->rowbytes + x + 2] = info_ptr->row_pointers[y][x+2];
-                    data[y*info_ptr->rowbytes + x + 3] = info_ptr->row_pointers[y][x+3];
-                }
-            }
             GLuint tex_id;
             glGenTextures(1, &tex_id);
             glBindTexture(GL_TEXTURE_2D, tex_id);
-            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, w, h, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+            auto size = GL_UNSIGNED_BYTE;
+            if(info_ptr->bit_depth == 16)
+                size = GL_UNSIGNED_SHORT;
+            if(info_ptr->bit_depth == 32)
+                size = GL_UNSIGNED_INT;
+            auto format = GL_RGB;
+            switch(info_ptr->color_type) {
+                case PNG_COLOR_TYPE_RGB:
+                    format = GL_RGB;
+                    break;
+                case PNG_COLOR_TYPE_RGBA:
+                    format = GL_RGBA;
+                    break;
+                case PNG_COLOR_TYPE_PALETTE:
+                    format = GL_RGB;
+                    size = GL_UNSIGNED_BYTE;
+                    break;
+                case PNG_COLOR_TYPE_GRAY:
+                    png_destroy_read_struct(&png_ptr, &info_ptr, &end_info);
+                    throw runtime_error("Gray PNG format not supported");
+                case PNG_COLOR_TYPE_GRAY_ALPHA:
+                    png_destroy_read_struct(&png_ptr, &info_ptr, &end_info);
+                    throw runtime_error("Gray Alpha PNG format not supported");
+                default:
+                    png_destroy_read_struct(&png_ptr, &info_ptr, &end_info);
+                    throw runtime_error(fmt::format("Format #{} not supported", info_ptr->color_type));
+            }
+            auto w = info_ptr->width;
+            auto h = info_ptr->height;
+            auto stride = info_ptr->pixel_depth/8;
+            unsigned char *data;
+            if(info_ptr->color_type == PNG_COLOR_TYPE_PALETTE)
+                data = new unsigned char[3*h*info_ptr->rowbytes];
+            else
+                data = new unsigned char[h*info_ptr->rowbytes];
+            for(int y = 0; y < h; y++) {
+                for(int x = 0; x < info_ptr->rowbytes; x += stride) {
+                    if(info_ptr->color_type == PNG_COLOR_TYPE_PALETTE) {
+                        unsigned char index = info_ptr->row_pointers[y][x];
+                        auto pixel = info_ptr->palette[index];
+                        data[3 * (y * info_ptr->rowbytes + x)] = pixel.red;
+                        data[3 * (y * info_ptr->rowbytes + x) + 1] = pixel.green;
+                        data[3 * (y * info_ptr->rowbytes + x) + 2] = pixel.blue;
+                    } else {
+                        for (int i = 0; i < stride; i++)
+                            data[y * info_ptr->rowbytes + x + i] = info_ptr->row_pointers[y][x + i];
+                    }
+                }
+            }
+
+            glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, w, h, 0, format, size, data);
             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+
+            // cleanup
             png_destroy_read_struct(&png_ptr, &info_ptr, &end_info);
-            delete data;
+            delete[] data;
+
             return tex_id;
         }
         std::string message = fmt::format("File header at {0} does not match png", path);

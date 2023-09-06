a02b5f1d2197476b2c96f9795d775019b3ecce78
Philip Smith <smithy545@gmail.com>
2023-09-06 14:33:55 -0500

    Move to short nonconflicting macro for Ptr definition

--- include/utils/macros.h
+++ include/utils/macros.h
@@ -27,12 +27,9 @@ SOFTWARE.
 
 #include <memory>
 
-#ifndef PTR
-#define PTR(CLASS) typedef std::shared_ptr<CLASS> Ptr
-#endif
 
-#ifndef USING_PTR
-#define USING_PTR(CLASS) using Ptr = std::shared_ptr<CLASS>
+#ifndef USEPTR
+#define USEPTR(CLASS) using Ptr = std::shared_ptr<CLASS>
 #endif
 
 #ifndef PLACEHOLDER
@@ -45,72 +42,6 @@ SOFTWARE.
 
 #ifndef ID
 #define ID(X) X
-#endif
-
-// Private var base
-#ifndef MY_DECLARE
-#define MY_DECLARE(TYPE, NAME, ACCESS) ACCESS: TYPE m_##NAME
-#endif
-
-#ifndef MY_VAR
-#define MY_VAR(TYPE, NAME, GETTER, GET_ACCESS, SETTER, SET_ACCESS) GETTER(NAME, TYPE, GET_ACCESS) SETTER(NAME, TYPE, SET_ACCESS) MY_DECLARE(TYPE, NAME, private)
-#endif
-
-// Base getter/setter
-#ifndef GETFUNC
-#define GETFUNC(NAME, TYPE, ACCESS, PRE, PREFIX, POST) ACCESS: PRE TYPE PREFIX##_##NAME() POST { return m_##NAME; }
-#endif
-
-#ifndef SETFUNC
-#define SETFUNC(NAME, TYPE, ACCESS, PRE, PREFIX, VALUE_MOD, ARG_MOD) ACCESS: PRE void PREFIX##_##NAME(TYPE ARG_MOD(val)) { m_##NAME = VALUE_MOD(val); }
-#endif
-
-// Useful Getter/Setter permutations
-#ifndef GET
-#define GET(NAME, TYPE, ACCESS) GETFUNC(NAME, TYPE, ACCESS, inline, get, const)
-#endif
-
-#ifndef GET_BOOL
-#define GET_BOOL(NAME, _, ACCESS) GETFUNC(NAME, bool, ACCESS, inline, is,  const)
-#endif
-
-#ifndef GET_CREF
-#define GET_CREF(NAME, TYPE, ACCESS) GETFUNC(NAME, const TYPE, ACCESS, inline, &get, const)
-#endif
-
-#ifndef SET
-#define SET(NAME, TYPE, ACCESS) SETFUNC(NAME, TYPE, ACCESS, inline, set, ID, ID)
-#endif
-
-// Useful permutations of var base
-#ifndef VAR
-#define VAR(TYPE, NAME, GET_ACCESS, SET_ACCESS) MY_VAR(TYPE, NAME, GET, GET_ACCESS, SET, SET_ACCESS)
-#endif
-
-#ifndef VAR_GET
-#define VAR_GET(TYPE, NAME, ACCESS) MY_VAR(TYPE, NAME, GET, ACCESS, NOOP, PLACEHOLDER)
-#endif
-
-#ifndef VAR_SET
-#define VAR_SET(TYPE, NAME, ACCESS) MY_VAR(TYPE, NAME, NOOP, PLACEHOLDER, SET, ACCESS)
-#endif
-
-// BVAR: boolean var gets use "is_X" instead of "get_X"
-#ifndef BVAR
-#define BVAR(NAME, GET_ACCESS, SET_ACCESS) MY_VAR(bool, NAME, GET_BOOL, GET_ACCESS, SET, SET_ACCESS)
-#endif
-
-#ifndef BVAR_GET
-#define BVAR_GET(NAME, ACCESS) MY_VAR(bool, NAME, GET_BOOL, ACCESS, NOOP, PLACEHOLDER)
-#endif
-
-// CREFVAR: var that is accessed with constant refs
-#ifndef CREFVAR
-#define CREFVAR(TYPE, NAME, GET_ACCESS, SET_ACCESS) MY_VAR(TYPE, NAME, GET_CREF, GET_ACCESS, SET_REF, SET_ACCESS)
-#endif
-
-#ifndef CREFVAR_GET
-#define CREFVAR_GET(TYPE, NAME, ACCESS) MY_VAR(TYPE, NAME, GET_CREF, ACCESS, NOOP, PLACEHOLDER)
 #endif
 
 #endif //UTILS_MACROS_H
